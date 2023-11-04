a2fa297c0db76269711cd031a74302e3d3251716
Kim Se-Un <kseun1949@gmail.com>
2023-11-04 16:09:09 +0300

    fix: внести правки в файл constants

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,5 +1,6 @@
 const PORT = process.env.PORT || 3000;
-const { MONGO_PATH } = process.env;
+const MONGO_PATH = process.env.MONGO_PATH || 'mongodb://127.0.0.1:27017/mestodb';
+const NODE_ENV = process.env.NODE_ENV || 'production';
 
 const ALLOWED_CORS = [
   'http://localhost:3000',
@@ -8,11 +9,12 @@ const ALLOWED_CORS = [
 ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-const KEY_SECRET = process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'key-secret';
+const KEY_SECRET = NODE_ENV === 'production' ? process.env.JWT_SECRET : 'key-secret';
 
 module.exports = {
   PORT,
   MONGO_PATH,
+  NODE_ENV,
   ALLOWED_CORS,
   DEFAULT_ALLOWED_METHODS,
   KEY_SECRET,
